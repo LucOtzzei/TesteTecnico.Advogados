23d92ddfaf2dfca9f6c6a40114123b29a091900f
lucas_ls <lucas.santos@fasters.com.br>
2025-10-26 13:56:49 -0300

    feat(advogado): criando enum de senioridade

---
+++ Otzzei.TesteTecnico.Dominio/Advogado/SenioridadeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otzzei.TesteTecnico.Dominio.Advogado
+{
+    public enum SenioridadeEnum
+    {
+        Junior = 1,
+        Pleno = 2,
+        Senior = 3
+    }
+}
